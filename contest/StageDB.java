e9cb21753a3cd19f95749e814d09000c10079288
manaty <166225524+mint00001@users.noreply.github.com>
2025-01-17 13:55:59 +0900

    Add files via upload

--- contest/StageDB.java
+++ contest/StageDB.java
@@ -18,9 +18,13 @@ class StageDB {
     static private Stage gameOverStage = null;
     static private MediaPlayer mainSound = null;
     static private MediaPlayer gameOverSound = null;
+    static private MediaPlayer healSound = null;
+    static private MediaPlayer damageSound = null;
     static private Class mainClass;
-    static private final String mainSoundFileName = "sound/JoyToTheWorld.mp3"; // BGM by OtoLogic
-
+    static private final String mainSoundFileName = "sound/bgm1.mp3"; // BGM by OtoLogic
+    static private final String gameOverSoundFileName = "sound/miss1.mp3";
+    static private final String healSoundFileName = "sound/maou_se_sound_drink01.mp3";
+    static private final String damageSoundFileName = "sound/maou_damage.mp3";
     public static void setMainClass(Class mainClass) {
         StageDB.mainClass = mainClass;
     }
@@ -32,7 +36,7 @@ class StageDB {
                 MediaPlayer mp = new MediaPlayer(m);
                 mp.setCycleCount(MediaPlayer.INDEFINITE); // loop play
                 mp.setRate(1.0); // 1.0 = normal speed
-                mp.setVolume(0.5); // volume from 0.0 to 1.0
+                mp.setVolume(0.7); // volume from 0.0 to 1.0
                 mainSound = mp;
             } catch (Exception io) {
                 System.err.print(io.getMessage());
@@ -44,12 +48,47 @@ class StageDB {
     public static MediaPlayer getGameOverSound() {
         if (gameOverSound == null) {
             try {
-                // please write down the code for playing gameover sound
+                Media m = new Media(new File(gameOverSoundFileName).toURI().toString());
+                MediaPlayer mp = new MediaPlayer(m);
+                mp.setCycleCount(MediaPlayer.INDEFINITE); // loop play
+                mp.setRate(1.0); // 1.0 = normal speed
+                mp.setVolume(0.7); // volume from 0.0 to 1.0
+                gameOverSound = mp;
             } catch (Exception io) {
                 System.err.print(io.getMessage());
             }
         }
         return gameOverSound;
+    }
+
+    public static MediaPlayer healSound() {
+        if (healSound == null) {
+            try {
+                Media m = new Media(new File(healSoundFileName).toURI().toString());
+                MediaPlayer mp = new MediaPlayer(m);
+                mp.setRate(1.0); // 1.0 = normal speed
+                mp.setVolume(0.7); // volume from 0.0 to 1.0
+                healSound = mp;
+            } catch (Exception io) {
+                System.err.print(io.getMessage());
+            }
+        }
+        return healSound;
+    }
+
+    public static MediaPlayer damageSound() {
+        if (damageSound == null) {
+            try {
+                Media m = new Media(new File(damageSoundFileName).toURI().toString());
+                MediaPlayer mp = new MediaPlayer(m);
+                mp.setRate(1.0); // 1.0 = normal speed
+                mp.setVolume(0.7); // volume from 0.0 to 1.0
+                damageSound = mp;
+            } catch (Exception io) {
+                System.err.print(io.getMessage());
+            }
+        }
+        return damageSound;
     }
 
     public static Stage getMainStage() {
