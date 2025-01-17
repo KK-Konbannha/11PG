5c111036e9d7c992cef822aef485ed11336e825e
KK-Konbannha <katsuk.618@gmail.com>
2025-01-17 14:40:06 +0900

    Changes

--- contest/MapGame.java
+++ contest/MapGame.java
@@ -1,8 +1,5 @@
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
 
 public class MapGame extends Application {
   Stage stage;
--- contest/MapGameController.java
+++ contest/MapGameController.java
@@ -2,15 +2,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
-import javafx.scene.Group;
-import javafx.scene.layout.Pane;
 import javafx.fxml.FXML;
 
 public class MapGameController implements Initializable {
@@ -43,13 +38,37 @@ public class MapGameController implements Initializable {
                 int index = y * mapData.getWidth() + x;
                 if (x == cx && y == cy) {
                     mapGrid.add(c.getCharaImageView(), x, y);
-                } else {
+
+                    if (mapData.getItems()[cy][cx] == MapData.TYPE_ITEM_DIG) {
+                        System.out.println("You found a dig item!");
+                        reDrawMap();
+                        return;
+                    }
+                    
+                } else if(mapData.getItemImageView(x,y) != null) {
+                    mapGrid.add(mapData.getItemImageView(x, y),x,y);
+                } else  {
                     mapGrid.add(mapImageViews[index], x, y);
                 }
             }
         }
     }
 
+    public void reDrawMap() {
+        mapData = new MapData(21, 15);
+        chara = new MoveChara(1, 1, mapData);
+        mapImageViews = new ImageView[mapData.getHeight() * mapData.getWidth()];
+        for (int y = 0; y < mapData.getHeight(); y ++) {
+            for (int x = 0; x < mapData.getWidth(); x ++) {
+                int index = y * mapData.getWidth() + x;
+                mapImageViews[index] = mapData.getImageView(x, y);
+            }
+        }
+        drawMap(chara, mapData);
+    }
+    
+    
+    
     // Get users' key actions
     public void keyAction(KeyEvent event) {
         KeyCode key = event.getCode();
@@ -65,12 +84,14 @@ public class MapGameController implements Initializable {
         }
     }
 
+    
     // Operations for going the cat up
     public void upButtonAction() {
         printAction("UP");
         chara.setCharaDirection(MoveChara.TYPE_UP);
         chara.move(0, -1);
         drawMap(chara, mapData);
+        
     }
 
     // Operations for going the cat down
@@ -79,6 +100,7 @@ public class MapGameController implements Initializable {
         chara.setCharaDirection(MoveChara.TYPE_DOWN);
         chara.move(0, 1);
         drawMap(chara, mapData);
+        
     }
 
     // Operations for going the cat right
@@ -87,6 +109,7 @@ public class MapGameController implements Initializable {
         chara.setCharaDirection(MoveChara.TYPE_LEFT);
         chara.move(-1, 0);
         drawMap(chara, mapData);
+       
     }
 
     // Operations for going the cat right
@@ -95,8 +118,10 @@ public class MapGameController implements Initializable {
         chara.setCharaDirection(MoveChara.TYPE_RIGHT);
         chara.move(1, 0);
         drawMap(chara, mapData);
+       
     }
 
+    
     @FXML
     public void func1ButtonAction(ActionEvent event) {
         try {
@@ -111,7 +136,17 @@ public class MapGameController implements Initializable {
 
     @FXML
     public void func2ButtonAction(ActionEvent event) {
-        System.out.println("func2: Nothing to do");
+        System.out.println("func2: Redraw Map!!");
+        mapData = new MapData(21, 15);
+        chara = new MoveChara(1, 1, mapData);
+        mapImageViews = new ImageView[mapData.getHeight() * mapData.getWidth()];
+        for (int y = 0; y < mapData.getHeight(); y ++) {
+            for (int x = 0; x < mapData.getWidth(); x ++) {
+                int index = y * mapData.getWidth() + x;
+                mapImageViews[index] = mapData.getImageView(x, y);
+            }
+        }
+        drawMap(chara, mapData);
     }
 
     @FXML
@@ -122,6 +157,7 @@ public class MapGameController implements Initializable {
     @FXML
     public void func4ButtonAction(ActionEvent event) {
         System.out.println("func4: Nothing to do");
+        
     }
 
     // Print actions of user inputs
